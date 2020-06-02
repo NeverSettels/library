c16680712e4addddd4e6ea27c35aadca196774b7
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-06-02 14:14:51 -0700

    build and debug Create pages for Authors and Books

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Linq;
 using Library.Models;
+using System;
 
 namespace Library.Controllers
 {
@@ -18,6 +20,27 @@ namespace Library.Controllers
     {
       List<Book> model = _db.Books.ToList();
       return View(model);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      ViewBag.Authors = _db.Authors.ToList();
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Book book, int AuthorId)
+    {
+      if (AuthorId != 0)
+      {
+        _db.Books.Add(book);
+        _db.SaveChanges();
+      }
+
+      Book newBook = _db.Books.FirstOrDefault(b => (b.Title == book.Title));
+      _db.AuthorBook.Add(new AuthorBook() { AuthorId = AuthorId, BookId = newBook.BookId });
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
